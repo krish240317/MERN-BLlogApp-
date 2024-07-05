ba8769f71dc30a3ace7eba88d336f6da12dc9f07
krish240317 <krishvaghela1710@gmail.com>
2024-07-05 06:40:58 +0530

    SignUp Page

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+//import components
+import Login from './Components/account/login';
+
+function App() {
+  return (
+    <div className="App" style={{marginTop:64}}>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
